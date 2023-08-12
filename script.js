4c40b2f6f6e263dc4fe28e74aa8367ba45787f21
HeitorBarbosaG <132144330+HeitorBarbosaG@users.noreply.github.com>
2023-08-12 18:25:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+    let numLivro = prompt("Digite o número do livro")
+    if(parseInt(numLivro) === 1){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Ru%C3%ADnas-Gorlan/dp/8539501090')
+    } else if(parseInt(numLivro) === 2){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Ponte-Chamas/dp/857676282X')
+    } else if(parseInt(numLivro) === 3){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Terra-Gelo/dp/8576763222')
+    } else if(parseInt(numLivro) === 4){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Folha-Carvalho/dp/8576763869')
+    } else if(parseInt(numLivro) === 5){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Feiticeiro-Norte/dp/8576767481')
+    } else if(parseInt(numLivro) === 6){
+        document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Cerco-Macindaw/dp/8576768933')
+    } else if(parseInt(numLivro) === 7){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Resgate-Erak/dp/8576768631')
+} else if(parseInt(numLivro) === 8){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Reis-Clonmel/dp/8539501287')
+} else if(parseInt(numLivro) === 9){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Halt-Perigo/dp/8539503417')
+} else if(parseInt(numLivro) === 10){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Imperador-Niho/dp/8539504774')
+} else if(parseInt(numLivro) === 11){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-Arqueiros-Hist%C3%B3rias-Perdidas/dp/8539505371')
+} else if(parseInt(numLivro) === 12){
+    document.write('https://www.amazon.com.br/Rangers-Ordem-dos-Arqueiros-Arqueiro/dp/8539509407')
+} else{
+    document.write('A coleção tem apenas 12 livros, recarregue a página e tente novamente')
+}
